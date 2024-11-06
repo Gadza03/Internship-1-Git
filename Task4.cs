9d2a8582ce55929d27554c8b6b058afdb31a6625
Gadza03 <mgadza@pmfst.hr>
2024-11-06 22:54:15 +0100

    Commit-anje cetvrtog zadatka

---
+++ Task4.cs
@@ -0,0 +1,36 @@
+﻿namespace Task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Unesite prirodni broj n (neparan broj): ");
+        int n;
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out n))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Neispravan unos.");
+            }
+
+        }
+
+
+        for (int i = 1; i <= n; i += 2)
+        {
+            Console.Write(new string(' ', (n - i) / 2));
+            Console.WriteLine(new string('*', i));
+        }
+
+        for (int i = n - 2; i >= 1; i -= 2)
+        {
+            Console.Write(new string(' ', (n - i) / 2));
+            Console.WriteLine(new string('*', i));
+        }
+    }
+}
